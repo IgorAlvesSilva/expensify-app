22b59dfaa9a3c55bbf19a896a263abda26f34f83
Igor <igor.alves@automni.com>
2020-09-15 04:21:11 +0000

    fix sort by amount

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -59,7 +59,7 @@ export class ExpenseListFilters extends React.Component{
 const mapDispatchToProps = (dispatch) => ({
   setTextFilter: (text) => dispatch(setTextFilter(text)),
   sortByDate: () => dispatch(sortByDate()),
-  sortByAmount: () => dispatch(setTextFilter()),
+  sortByAmount: () => dispatch(sortByAmount()),
   setStartDate: (startDate) => dispatch(setStartDate(startDate)),
   setEndDate: (endDate) => dispatch(setEndDate(endDate)),
 });
